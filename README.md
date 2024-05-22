979c18f3e871e3dc024e1a16301c7559b0a5f0aa
gstvo2k15 <48087822+gstvo2k15@users.noreply.github.com>
2024-05-22 21:30:13 +0200

    Added initial files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# T800
-OpenAI_T800
+# OpenAI_T800
+
+Una vez en el directorio del script, ejecútalo utilizando el comando python seguido del nombre del archivo Python:
+
+`python T800.py`
+
+Esto ejecutará el script de Python y comenzará su funcionamiento según lo que hayas programado.
+Si el script depende de bibliotecas específicas que no están instaladas, puedes usar pip (el administrador de paquetes de Python) para instalar las bibliotecas necesarias.
---
+++ t800.py
@@ -0,0 +1,51 @@
+import speech_recognition as sr
+from playsound import playsound
+import os
+
+# Ruta donde se encuentran los archivos de audio
+sounds = os.path.join(current_dir, 'T800')
+
+
+# Diccionario de preguntas y respuestas asociadas a archivos de audio
+respuestas = {
+    "T800 que modelo eres": "Cyberdine.mp3",
+    "T800 de que estas hecho": "human_esqueleton.mp3",
+    "T800 quien te envia": "35years.mp3",
+    "despidete": "sayonara.mp3",
+    "descansa": "beback.mp3",
+    "comemela": "calmdown.mp3"
+}
+
+# Inicializar el reconocedor de voz
+recognizer = sr.Recognizer()
+
+# Funcion para obtener la pregunta del usuario y reproducir la respuesta
+def obtener_respuesta():
+    with sr.Microphone() as source:
+        print("Di tu pregunta ('T800 que modelo eres', 'T800 de que estas hecho', etc.):")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    try:
+        # Reconocer la pregunta del usuario utilizando Google Speech Recognition
+        pregunta = recognizer.recognize_google(audio, language="es-ES")
+        print(f"Pregunta: {pregunta}")
+
+        # Verificar si la pregunta esta en el diccionario de respuestas
+        if pregunta in respuestas:
+            archivo_respuesta = respuestas[pregunta]
+            ruta_completa = os.path.join(ruta_archivos, archivo_respuesta)
+            print(f"Respuesta: {ruta_completa}")
+            # Reproducir el archivo de audio asociado a la pregunta
+            playsound(ruta_completa)
+        else:
+            print("Lo siento, no tengo una respuesta para esa pregunta.")
+
+    except sr.UnknownValueError:
+        print("No se pudo entender la pregunta.")
+    except sr.RequestError:
+        print("Error al realizar la solicitud de reconocimiento de voz.")
+
+# Ejecutar la funcion para obtener la pregunta y reproducir la respuesta
+obtener_respuesta()
+
